1588acc986b7203a11f2cccbed28861e79f9cfd9
Sreyas <sreyastejas777@gmail.com>
2026-07-20 19:39:56 +0530

    chore: Update base path for Technology-Calor-Mega deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CALORMEGA-chng/',
+  base: '/Technology-Calor-Mega/',
 })
